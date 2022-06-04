a4b6270cecce95aaf0e6f94c8e9ed2edffeeba2e
Danilo Madrigalejos <dan@yahoo.com>
2022-06-04 10:56:07 +0800

    working na ung X =O

--- tic_tac_toe_vb/Form1.vb
+++ tic_tac_toe_vb/Form1.vb
@@ -2,38 +2,102 @@
     Dim turn As Char = "X"
 
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
-        btn1.Text = turn
+        If btn1.Text = "" Then
+            btn1.Text = turn
+
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
-        btn2.Text = turn
+        If btn2.Text = "" Then
+            btn2.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
-        btn3.Text = turn
+        If btn3.Text = "" Then
+            btn3.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
-        btn4.Text = turn
+        If btn4.Text = "" Then
+            btn4.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
-        btn5.Text = turn
+        If btn5.Text = "" Then
+            btn5.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
-        btn6.Text = turn
+        If btn6.Text = "" Then
+            btn6.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
-        btn7.Text = turn
+        If btn7.Text = "" Then
+            btn7.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
-        btn8.Text = turn
+        If btn8.Text = "" Then
+            btn8.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 
     Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
-        btn9.Text = turn
+        If btn9.Text = "" Then
+            btn9.Text = turn
+            If turn = "X" Then
+                turn = "O"
+            Else
+                turn = "X"
+            End If
+        End If
     End Sub
 End Class
